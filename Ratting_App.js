24d3a969959273372b73c63b9cf01ba8d094648e
amir1499 <91052159+amir1499@users.noreply.github.com>
2023-04-11 22:51:13 +0530

    Add files via upload
    Rating App Example

---
+++ Ratting_App.js
@@ -0,0 +1,13 @@
+var count;
+function starmark(item){
+  count = item.id[0];
+  sessionStorage.starRating = count;
+  var subid = item.id.substring(1);
+  for(var i = 0; i < 5; i++){
+    if(i < count){
+      document.getElementById((i + 1) + subid).style.color = "#FDE16D";
+    }else{
+      document.getElementById((i+1)+subid).style.color="white";
+    }
+  }
+}
